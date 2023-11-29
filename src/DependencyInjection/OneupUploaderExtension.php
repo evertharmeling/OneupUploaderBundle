817a03196048efef901611dcc3df1ae4c6b038ee
Evert Harmeling <evertharmeling@gmail.com>
2023-11-29 09:40:52 +0100

    Refactored phpstan fixes

--- src/DependencyInjection/OneupUploaderExtension.php
+++ src/DependencyInjection/OneupUploaderExtension.php
@@ -285,17 +285,7 @@ class OneupUploaderExtension extends Extension
             ->addArgument($prefix);
     }
 
-<<<<<<< HEAD
-<<<<<<< HEAD
     protected function getMaxUploadSize(mixed $input): int
-=======
-=======
-    /**
-     * @param mixed $input
-     */
->>>>>>> ab844e1 (Fixes phpstan errors)
-    protected function getMaxUploadSize($input): int
->>>>>>> 2b60727 (Updated php_cs_fixer to v4)
     {
         $input = $this->getValueInBytes($input);
         $maxPost = $this->getValueInBytes(\ini_get('upload_max_filesize'));
@@ -308,24 +298,10 @@ class OneupUploaderExtension extends Extension
         return min(min($input, $maxPost), $maxFile);
     }
 
-<<<<<<< HEAD
-<<<<<<< HEAD
     protected function getValueInBytes(mixed $input): int
-=======
-=======
-    /**
-     * @param mixed $input
-     */
->>>>>>> ab844e1 (Fixes phpstan errors)
-    protected function getValueInBytes($input): int
->>>>>>> 2b60727 (Updated php_cs_fixer to v4)
     {
         // see: http://www.php.net/manual/en/function.ini-get.php
-<<<<<<< HEAD
         if (!\is_scalar($input)) {
-=======
-        if (!is_scalar($input)) {
->>>>>>> ab844e1 (Fixes phpstan errors)
             return -1;
         }
 
--- src/Uploader/Chunk/ChunkManagerInterface.php
+++ src/Uploader/Chunk/ChunkManagerInterface.php
@@ -10,26 +10,19 @@ interface ChunkManagerInterface
 {
     /**
      * Adds a new Chunk to a given uuid.
-     *
-     * @return mixed
      */
     public function addChunk(string $uuid, int $index, UploadedFile $chunk, string $original): mixed;
 
     /**
      * Assembles the given chunks and return the resulting file.
      *
-     * @param mixed $chunks
      * @param bool $removeChunk remove the chunk file once its assembled
      * @param bool $renameChunk rename the chunk file once its assembled
-     *
-     * @return mixed
      */
     public function assembleChunks(mixed $chunks, bool $removeChunk = true, bool $renameChunk = false): mixed;
 
     /**
      * Get chunks associated with the given uuid.
-     *
-     * @return mixed
      */
     public function getChunks(string $uuid): mixed;
 
